22bffa0cf2286dc7775913af2a97eecf621f262c
satyarjs <75620061+satyarjs@users.noreply.github.com>
2022-12-07 17:54:37 +0530

    Add files via upload

---
+++ Covid-Tracker-main/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import React from 'react';
+
+import { Cards, CountryPicker, Charts } from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+
+
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <h1>Covid 19 - Pandemic</h1>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Charts data={data} country={country} /> 
+      </div>
+    );
+  }
+}
+
+export default App;
